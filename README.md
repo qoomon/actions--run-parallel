e5c2bedb33e4546de91c0497e65ec815de2c900d
Bengt Brodersen <qoo@qoomon.me>
2025-04-20 15:24:19 +0200

    chore: init

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
+# Parallel Steps
 
-
-
-## Installation
-```shell
-gh extension install https://github.com/nektos/gh-act
-```
-
-
-```shell
-gh act push
-gh act -l pull_request
-
-act -P ubuntu-latest=-self-hosted
-act -P windows-latest=-self-hosted
-act -P macos-latest=-self-hosted
-```
+With this action, you can run parallel steps in a GitHub Actions workflow jobs.
---
+++ main.js
@@ -0,0 +1,179 @@
+import core from '@actions/core';
+import github from '@actions/github';
+import YAML from "yaml";
+import child_process from 'node:child_process';
+import fs from 'node:fs';
+import path from 'node:path';
+import chalk from 'chalk';
+
+chalk.level = 2;
+
+const githubToken = core.getInput("token", { required: true });
+// Get steps from input
+const steps = YAML.parse(core.getInput("steps", { required: true }));
+// TODO validate steps
+if (!Array.isArray(steps)) {
+  throw new Error("steps must be an array");
+}
+
+// Install gh-act extension
+child_process.execSync("gh extension install https://github.com/nektos/gh-act", {
+  env: {
+    ...process.env,
+    GH_TOKEN: githubToken,
+  },
+});
+// Run all steps in parallel
+await runStepsInParallel(steps).catch(() => {
+  core.setFailed("One or more parallel steps failed");
+});
+
+// ----------------------------------------------------------------
+//
+function runStepsInParallel(steps) {
+  const worklfowFile = `${process.env.RUNNER_TEMP ?? '/tmp'}/${github.context.action}.yaml`;
+  const workflow = {
+    on: "workflow_dispatch",
+    jobs:  Object.assign({}, ...steps.map((step, index) => ({
+      [`Step${index}`]: {
+        "runs-on": "host",
+        "steps": [step]
+      }
+    })))
+  };
+  fs.mkdirSync(path.dirname(worklfowFile), { recursive: true });
+  fs.writeFileSync(worklfowFile, YAML.stringify(workflow));
+
+  core.startGroup("Output");
+
+  const workflowProcess = child_process.spawn("gh", [
+    "act",
+    "--workflows", worklfowFile,
+    "--platform", "host=-self-hosted",
+    "--action-offline-mode",
+    "--log-prefix-job-id",
+    "--actor", github.context.actor,
+    "-s", `GITHUB_TOKEN=${githubToken}`
+    // "--env-file", envFilePath,
+    // "--eventpath", eventFilePath
+  ], {
+    env: {
+      ...process.env,
+    },
+  });
+
+
+  return new Promise((resolve, reject) => {
+    let stepOutput = Object.fromEntries(Object.keys(workflow.jobs).map(jobId => [jobId, ""]));
+
+    let stdoutChunkStack = "";
+    workflowProcess.stdout.on("data", (chunk) => {
+      stdoutChunkStack += chunk.toString();
+      const stdoutChunkStackLines = stdoutChunkStack.split("\n")
+      for (const line of stdoutChunkStackLines.slice(0, -1)) {
+        const parsedLine = parseLine(line);
+        if(parsedLine.level == 'info') {
+          process.stdout.write(parsedLine.line);
+          stepOutput[parsedLine.jobId] += parsedLine.line;
+        }
+      }
+
+      stdoutChunkStack = stdoutChunkStackLines.slice(-1)[0] ?? "";
+    });
+
+    let stderrChunkStack = "";
+    workflowProcess.stderr.on("data", (chunk) => {
+      stderrChunkStack += chunk.toString();
+      const stderrChunkStackLines = stderrChunkStack.split("\n")
+      for (const line of stderrChunkStackLines.slice(0, -1)) {
+        const parsedLine = parseLine(line);
+        if(parsedLine.level == 'info') {
+          process.stderr.write(parsedLine.line);
+          stepOutput[parsedLine.jobId] += parsedLine.line;
+        }
+      }
+
+      stderrChunkStack = stderrChunkStackLines.slice(-1)[0] ?? "";
+    });
+
+    // TODO https://stackoverflow.com/questions/37522010/difference-between-childprocess-close-exit-events
+    // p.on("close", resolve);
+    workflowProcess.on("exit", (exitCode) => {
+      const parsedStdoutChunkStack = parseLine(stdoutChunkStack);
+      if(parsedStdoutChunkStack.level == 'info') {
+        process.stdout.write(parsedStdoutChunkStack.line);
+         stepOutput[parsedStdoutChunkStack.jobId] += parsedStdoutChunkStack.line;
+      }
+
+      const parsedStderrtChunkStack = parseLine(stderrChunkStack);
+      if(parsedStderrtChunkStack.level == 'info') {
+        process.stderr.write(parsedStderrtChunkStack.line);
+        stepOutput[parsedStderrtChunkStack.jobId] += parsedStderrtChunkStack.line;
+      }
+
+      core.endGroup();
+
+      // grouped output
+      console.log('');
+      Object.entries(stepOutput).forEach(([jobId, output]) => {
+
+        core.startGroup(output.split('\n').slice(-2)[0]);
+        console.log(output);
+        core.endGroup();
+      });
+
+      if (exitCode !== 0){
+        reject();
+      } else {
+        resolve();
+      }
+    });
+
+    function parseLine(line) {
+      const rawLine = line.replaceAll(/\x1B\[\d+m/g, '');
+      const lineMatch = rawLine.trim().match(/^\[(?<jobId>Step\d+)]\s*(?<msg>.*)/)
+
+      if(!lineMatch) {
+        return {
+          jobId: null,
+          line
+        };
+      }
+
+      if(!(lineMatch.groups.msg.startsWith("⭐ Run Main")
+        || lineMatch.groups.msg.startsWith("|")
+        || lineMatch.groups.msg.startsWith("✅  Success - Main")
+        || lineMatch.groups.msg.startsWith("❌  Failure - Main"))) {
+        return {
+          jobId: null,
+          line
+        };
+      }
+
+      const jobId = lineMatch.groups.jobId;
+      const jobNumber = parseInt(jobId.match(/\d+$/)[0], 10);
+      const msg = lineMatch.groups.msg
+        .replace(/^⭐ Run Main/, '▶ Run - Main')
+        .replace(/^✅  Success/, '🟢 Success')
+        .replace(/^❌  Failure/, '🔴 Failure')
+        .replace(/^\|\s*/, '') ;
+
+      const stepColors = [
+        chalk.red,
+        chalk.green,
+        chalk.yellow,
+        chalk.blue,
+        chalk.magenta,
+        chalk.cyan,
+        chalk.white,
+      ];
+      const stepColor = stepColors[jobNumber % stepColors.length];
+
+      return {
+        jobId,
+        line: stepColor(`[${jobId}]`) + ` ${msg}\n`,
+        level: 'info'
+      }
+    }
+  });
+}
